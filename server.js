e4e30707c12465e58e1edbb3e7d2c0932890ed92
Shivam Singh <shivamcena07@gmail.com>
2020-11-23 15:17:05 +0530

    Errors

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ app.use('/api/posts', require('./config/routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+console.log(`Server started on port ${PORT}`);
