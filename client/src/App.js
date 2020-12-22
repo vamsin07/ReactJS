37dc9e101623263ddad9b778a9635c3be20b0f7b
Shivam Singh <shivamcena07@gmail.com>
2020-12-22 23:17:23 +0530

    Redux Actions

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,22 @@ import Navbar from './components/layout/navbar';
 import Landing from './components/landing';
 import Register from './components/auth/register';
 import Login from './components/auth/login';
+import Alert from './components/layout/Alert';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 
 const App = () => (
-    
-      <Router>
+       <Provider store={store}>
+         <Router>
       <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
@@ -22,6 +28,8 @@ const App = () => (
         </section>
       </Fragment>
       </Router>
+       </Provider>
+      
 );
   
 export default App;
