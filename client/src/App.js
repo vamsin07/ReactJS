c91a27fe993233a33a0a7ad598fa61fda0da27d9
Shivam Singh <shivamcena07@gmail.com>
2021-01-21 12:46:20 +0530

    Login Error

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-import { setAuthToken } from './utils/setAuthToken';
+import  setAuthToken  from './utils/setAuthToken';
 
 import './App.css';
 
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,9 @@ import  {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from './types';
 import setAuthToken from  '../utils/setAuthToken';
 
@@ -41,12 +43,13 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({name, email, password});
 
   try {
-    const res = await axios.post('/api/users', body, config);
+    const res = await axios.post('/api/user', body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -58,4 +61,36 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_FAIL
     });
   }
-}
+};
+
+// Login User
+export const login = ({email, password }) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({email, password});
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser()); 
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if(errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+      
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
