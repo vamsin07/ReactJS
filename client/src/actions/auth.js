4c7e1fa61ec781677e8629921c403884711cd0d0
Shivam Singh <shivamcena07@gmail.com>
2021-01-22 00:08:27 +0530

    Logout

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import  {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from  '../utils/setAuthToken';
 
@@ -89,3 +90,8 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
   };
+
+  // Logout / Clear Profile
+  export const logout = () => dispatch => {
+    dispatch({type: LOGOUT});
+  }
